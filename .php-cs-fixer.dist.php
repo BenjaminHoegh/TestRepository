95f67b9ac7393d2f156bf63470ac208b757cace5
Benjamin <bh@hawkon.dk>
2021-08-01 10:01:13 +0000

    updated cs-fixer

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,13 +1,27 @@
-<?php declare(strict_types=1);
+<?php 
+declare(strict_types=1);
+
 require __DIR__ . '/vendor/autoload.php';
 
 use Symfony\Component\Finder\Finder;
 
-$finder = (new Finder())
+
+$finder = PhpCsFixer\Finder::create()
     ->files()
     ->name('*.php')
     ->in(__DIR__)
     ->exclude('vendor');
 
-return (new Config())
-    ->setFinder($finder);
+$config = new PhpCsFixer\Config();
+$config
+    ->setRiskyAllowed(true)
+    ->setRules([
+        '@PSR12' => true,
+        '@PHP80Migration' => true,
+        '@PhpCsFixer' => true,
+        'general_phpdoc_annotation_remove' => ['annotations' => ['expectedDeprecation']],
+    ])
+    ->setFinder($finder)
+;
+
+return $config;
